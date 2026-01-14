9001192a4c40d342874647dd85e69460c60bcaba
WillyGrning <willygurning0@gmail.com>
2026-01-14 22:11:57 +0700

    pckg

--- server/index.js
+++ server/index.js
@@ -18,13 +18,13 @@ app.use(express.json());
 
 // Supabase client
 const supabase = createClient(
-  process.env.VITE_SUPABASE_URL,
-  process.env.VITE_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 
 const supabaseAdmin = createClient(
-  process.env.VITE_SUPABASE_URL,
-  process.env.VITE_SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 // ========== EMAIL TRANSPORTER ========== // <-- TAMBAH INI

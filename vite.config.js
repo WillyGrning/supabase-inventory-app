aa1d99cba00311e29293c8cddce6e0661a5b3633
WillyGrning <willygurning0@gmail.com>
2026-01-14 22:19:05 +0700

    pckg

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        // target: 'http://localhost:3001',
+        target: 'https://supabase-inventory-app-production.up.railway.app',
         changeOrigin: true,
         secure: false,
       }

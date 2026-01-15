5706b1a6389ee9b4d2831d21a98ef64e9c9016bb
WillyGrning <willygurning0@gmail.com>
2026-01-15 11:53:20 +0700

    fixed direct url

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
+      "/api": {
         // target: 'http://localhost:3001',
-        target: 'https://supabase-inventory-app-production.up.railway.app',
+        target: "https://supabase-inventory-app-production.up.railway.app",
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-})
+      },
+    },
+    historyApiFallback: true,
+  },
+});
